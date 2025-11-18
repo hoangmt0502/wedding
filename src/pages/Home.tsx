e5cdb02bca19210092d90620c01c2a4fdad52d04
hoangmt0502 <hoangmt0502@gmail.com>
2025-11-18 16:46:55 +0700

    main

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Box, Stack, Typography } from "@mui/material";
 import WeddingFallingImages from "../components/WeddingFallingImages";
+import { LIST_IMAGES_FALLING } from "../constants/common";
 
 export default function Home() {
   return (
@@ -10,15 +11,7 @@ export default function Home() {
           </Stack>
         </Stack>
         <WeddingFallingImages
-          images={[
-            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABGUlEQVRYR+2W0Q3CMAxFf5IiiaBqiKBqCCoYoGogqCGoIoGogqCGoI9kTJ2vnfuQTxmqbz3WYpKDcRzCA1pQcDqQmNfVCeGj6cqrVnB0n0lL5DHa+zJXfV8k5ofwUvQaSgAqfHkD8pM2HtYeQGPmCkABZ6s0vraOpiwdj87Qk+JxgA8aXzYyOKgJZB0s3h7HkPpA6dHZqGUxAHg+2gAAqKJsrlfwiOUImyAx6jTEtVJALzXXbF3Bno3HnbJdkGXz5wNi0lB8Z2oULGbcHgj0x9Fclo7E5BdTRbYoR5MD5gdp6J3NJCf0oVqkXPpUOo7W/5HtwjSddWJdcUFHGm+VxArWRkmM8JmfK0f8WWW8mZUuI/6gAAAABJRU5ErkJggg==",
-            "/images/autumn_leaves_PNG3602.png",
-            "/images/dove.png",
-            "/images/flower.png",
-            "/images/heart_PNG51315.png",
-            "/images/heart_PNG51342.png",
-            "/images/sparkle.png",
-          ]}
+          images={LIST_IMAGES_FALLING}
           doveImage="/img/dove.png"
           musicUrl="/audio/song.mp3"     // optional, để sync bass
           enableGlitter={true}
