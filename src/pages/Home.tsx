fa7041ac66c5d1946256f7e36286a1ebce7dff2d
hoangmt0502 <hoangmt0502@gmail.com>
2025-11-18 10:35:39 +0700

    init

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 export default function Home() {
   return (
     <Box mt={2} mb={1}>
       <Stack flexDirection={{xs: 'column', md: "row"}} gap={3}>
         <Stack flex={0.7} gap={3}>
+          <Typography>Gửi vợ</Typography>
         </Stack>
       </Stack>
     </Box>
