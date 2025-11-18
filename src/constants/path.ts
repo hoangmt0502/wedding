b85cf84d56d038b5477d432c4e2edc551f55fcbf
hoangmt0502 <hoangmt0502@gmail.com>
2025-11-18 10:44:12 +0700

    init

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,7 +1,3 @@
 export const PATH_NAME = {
   home: "/",
-  login: "/login",
-  user: "/user",
-  groups: "/groups",
-  permission: "/permission",
 };
--- src/locales/vi/index.ts
+++ src/locales/vi/index.ts
@@ -1,24 +1,8 @@
 
-import login from "./login";
 import common from "./common";
 import { helper } from "./helper";
-import sidebar from "./sidebar";
-import userManage from "./userManage";
-import roleManage from "./roleManage";
-import permissionManage from "./permissionManage";
-import groups from './user/groups';
-import subscriptionPackage from "./user/subscriptionPackage";
 
 const vi = {
-  login,
-  user: {
-    groups,
-    subscriptionPackage,
-  },
-  userManage,
-  roleManage,
-  permissionManage,
-  sidebar,
   common,
   helper,
 }
